ef36e74e36f93850b9d8b3f9fad8886ee809f618
AntonKomyakov <anton.komyakov@blueyonder.com>
2025-09-26 14:54:31 +0200

    Browser info added to the screen resolution test

--- js/initscreen.js
+++ js/initscreen.js
@@ -1,11 +1,17 @@
 (function($){
     $(function(){
-        let rContainer = $('#resolutionContainer')
+        let rContainer = $('#resolutionContainer');
+        let nContainer = $('#navigatorContainer');
       setInterval(updateResolution,1000, rContainer);
+      getBrowserInfo(nContainer);
     }); // end of document ready
     
 })(jQuery); // end of jQuery name space
   
+function getBrowserInfo(container) {
+    container.html(navigator.userAgent);
+}
+
 function updateResolution(container) {
     container.html(window.screen.width+"&times;"+window.screen.height+"<br />"+window.screen.availWidth+"&times;"+window.screen.availHeight);
 }
